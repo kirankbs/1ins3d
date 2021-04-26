fae7cae89ef1c42d335d526171d3021ce9104c53
Kiran Kumar <kirankumar@RLYLTPZ14.local>
2021-04-27 00:26:43 +0200

    Add basic war report functionality

--- commands/warreport.js
+++ commands/warreport.js
@@ -1,16 +1,38 @@
 const Discord = require('discord.js');
 
+const warreportjson = require('./warreport.json');
+
+function endsWith(str, suffix) {
+  return str.indexOf(suffix, str.length - suffix.length) !== -1;
+}
+
 module.exports = {
     name: 'warreport',
     execute(message, args){
-        
-        const embed = new Discord.MessageEmbed()
-        .setTitle('Squad War Report')
-        .setDescription('This report show war score and description')
-        .setImage('https://cdn.discordapp.com/attachments/801590191907274772/803342971922808882/1611602275739.jpg');
 
-        message.delete();
-        message.channel.send(embed);
+      const embed = new Discord.MessageEmbed()
+        .setTitle('Squad War Report')
+
+      message.delete();  
+
+      if(args[0] == "latest") {
+        const pos = (args[1] && Number(args[1]) > 1) ? (Number(args[1]) - 1) : 0
+        const {date, event, opps, score, image} = warreportjson[pos];
+        const [squadScoreR, opponentScoreR] = score.split('vs');
+        const squadScore = squadScoreR.trim().slice(-1) == "M" ? Number(squadScoreR.trim().slice(0, -1)) * 1000000 : Number(squadScoreR.trim().slice(0, -1)) * 1000;
+        const opponentScore = opponentScoreR.trim().slice(-1) == "M" ? Number(opponentScoreR.trim().slice(0, -1)) * 1000000 : Number(opponentScoreR.trim().slice(0, -1)) * 1000;
+
+        const result = squadScore > opponentScore ? "Victory" : "Defeat"
+        embed.setDescription(
+          `\ndate: ${date}`+
+          `\nevent: ${event}`+
+          `\nopps: ${opps}`+
+          `\nscore: ${score}`+
+          `\nresult: ${result}`
+          ).setImage(image)
+
+          message.channel.send(embed);
+      }
     }
 
 }
